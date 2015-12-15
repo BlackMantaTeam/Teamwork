453543543da7f31e5133e37e4675135b5b0c229f
zvet80 <zvet80@hotmail.com>
2015-12-15 16:37:11 +0200

    Initial solution structure

---
+++ MusicPlayer/MusicPlayer/Common/DelegateCommand.cs
@@ -0,0 +1,34 @@
+﻿namespace MusicPlayer.Common
+{
+    using System;
+    using System.Windows.Input;
+
+    public class DelegateCommand : ICommand
+    {
+        private Action execute;
+        private Func<bool> canExecute;
+
+        public DelegateCommand(Action execute,
+                   Func<bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (this.canExecute == null)
+            {
+                return true;
+            }
+            return this.canExecute();
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            this.execute();
+        }
+    }
+}
